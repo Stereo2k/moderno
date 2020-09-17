26bb56d41283d1c8cdfdaf10b602bc1ce155e92b
Stereo2k <stereo2k2013@ya.ru>
2020-09-18 01:48:14 +0300

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -6,13 +6,23 @@ $(function () {
 		readOnly: true
 	});
 
+
 	$('.product-slider__inner').slick({
 		arrows: false,
 		dots: true,
-		slidesToShow:4,
-		slidesToScroll:4,
-
+		slidesToShow: 4,
+		slidesToScroll: 4,
 	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+});
+
 
 
 	var mixer = mixitup('.products__inner-box');
